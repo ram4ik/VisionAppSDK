44835242132c36e402d86dc0a4bb4971ff256031
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-03-26 19:55:45 +0200

    init sdk

--- VisionAppSDK/VisionSDK.swift
+++ VisionAppSDK/VisionSDK.swift
@@ -6,13 +6,42 @@
 //
 
 import Foundation
+import Vision
+import UIKit
 
 public class VisionSDK {
     
     public init() {}
     
-    public func getRecognizedText() {
+    public func getRecognizedText(image: UIImage) {
         
-        print("It's from SDK")
+        // Get the CGImage on which to perform requests.
+        guard let cgImage = image.cgImage else { return }
+        
+        // Create a new image-request handler.
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+        
+        // Create a new request to recognize text.
+        let request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
+        
+        do {
+            // Perform the text-recognition request.
+            try requestHandler.perform([request])
+        } catch {
+            print("Unable to perform the requests: \(error).")
+        }
+    }
+    
+    func recognizeTextHandler(request: VNRequest, error: Error?) {
+        guard let observations =
+                request.results as? [VNRecognizedTextObservation] else {
+                    return
+                }
+        let recognizedStrings = observations.compactMap { observation in
+            // Return the string of the top VNRecognizedText instance.
+            return observation.topCandidates(1).first?.string
+        }
+        
+        print("Recognized text: \(recognizedStrings)")
     }
 }
