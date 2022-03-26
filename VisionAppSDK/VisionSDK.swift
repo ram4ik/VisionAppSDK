77146f227409856aaf0e0a878a510f4f9278dcc7
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-03-26 19:39:44 +0200

    init

---
+++ VisionAppSDK/VisionSDK.swift
@@ -0,0 +1,18 @@
+//
+//  VisionSDK.swift
+//  VisionAppSDK
+//
+//  Created by ramil on 26.03.2022.
+//
+
+import Foundation
+
+public class VisionSDK {
+    
+    public init() {}
+    
+    public func getRecognizedText() {
+        
+        print("It's from SDK")
+    }
+}
