7f3339f06362468569f1edfc0f60bfd56fa0b989
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-03-26 19:23:45 +0200

    Initial Commit

---
+++ VisionAppSDK/VisionAppSDK.h
@@ -0,0 +1,18 @@
+//
+//  VisionAppSDK.h
+//  VisionAppSDK
+//
+//  Created by ramil on 26.03.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VisionAppSDK.
+FOUNDATION_EXPORT double VisionAppSDKVersionNumber;
+
+//! Project version string for VisionAppSDK.
+FOUNDATION_EXPORT const unsigned char VisionAppSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisionAppSDK/PublicHeader.h>
+
+
